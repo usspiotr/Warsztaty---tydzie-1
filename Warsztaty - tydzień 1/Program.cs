bad6d5f4e33c2c381d479b67b66b11bf0a59b8c8
Piotr Uss <usspiotr.github@gmail.com>
2017-10-13 14:17:25 +0200

    Add project files.

---
+++ Warsztaty - tydzień 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Warsztaty___tydzień_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
